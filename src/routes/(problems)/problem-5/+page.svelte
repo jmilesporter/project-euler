00e0144837bd0361e770479b9ba17eb917a70232
Jeffrey Porter <jmilesporter@gmail.com>
2022-09-10 17:34:09 -0400

    Problem 5 and Problem 6

---
+++ src/routes/(problems)/problem-5/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+	function smallestDivisible(number) {
+		for (let i = 2520; i < 10000000000; i = i + 2520) {
+			let isDivisible = 0;
+			for (let j = 1; j <= number; j++) {
+				if (i % j !== 0) {
+					break;
+				} else {
+					isDivisible++;
+					console.log(`this is the number: ${i}`);
+					console.log(isDivisible);
+					if (isDivisible === number) {
+						return i;
+					}
+				}
+			}
+		}
+	}
+
+	let answer = smallestDivisible(20);
+</script>
+
+<h1>Problem 5</h1>
+
+<p>
+	2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any
+	remainder.
+</p>
+<p>
+	What is the smallest positive number that is <dfn title="divisible with no remainder"
+		>evenly divisible</dfn
+	> by all of the numbers from 1 to 20?
+</p>
+
+<p class="answer">{answer}</p>
+
+<style>
+	h1 {
+		text-align: center;
+		margin: 1.5em;
+	}
+	p {
+		text-align: center;
+	}
+	.answer {
+		text-align: center;
+		font-size: xxx-large;
+		align-items: center;
+	}
+</style>
---
+++ src/routes/(problems)/problem-6/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+	let array = [...Array(101).keys()];
+	array.splice(0, 1);
+
+	let sumOfSquares = array
+		.map((n) => {
+			return n * n;
+		})
+		.reduce((a, b) => {
+			return a + b;
+		});
+	let squareOfSums = Math.pow(
+		array.reduce((a, b) => {
+			return a + b;
+		}),
+		2
+	);
+	console.log(squareOfSums);
+	console.log(sumOfSquares);
+
+	let answer = squareOfSums - sumOfSquares;
+</script>
+
+<h1>Problem 6</h1>
+
+<p>The sum of the squares of the first ten natural numbers is, 1^2 + 2^2 + ... + 10^2 = 385</p>
+<p>The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)^2 = 55^2 = 3025</p>
+<p>
+	Hence the difference between the sum of the squares of the first ten natural numbers and the
+	square of the sum is 3025 - 385 = 2640.
+</p>
+<p>
+	Find the difference between the sum of the squares of the first one hundred natural numbers and
+	the square of the sum.
+</p>
+
+<p class="answer">{answer}</p>
+
+<style>
+	h1 {
+		text-align: center;
+		margin: 1.5em;
+	}
+	p {
+		text-align: center;
+	}
+	.answer {
+		text-align: center;
+		font-size: xxx-large;
+		align-items: center;
+	}
+</style>
