fb75d2ed4eab8d74ff39e23acbbd9278e01424ba
Jeffrey Porter <jmilesporter@gmail.com>
2022-09-10 17:39:41 -0400

    Problem 5 and Problem 6

--- src/routes/(problems)/problem-6/+page.svelte
+++ src/routes/(problems)/problem-6/+page.svelte
@@ -23,8 +23,8 @@
 
 <h1>Problem 6</h1>
 
-<p>The sum of the squares of the first ten natural numbers is, 1^2 + 2^2 + ... + 10^2 = 385</p>
-<p>The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)^2 = 55^2 = 3025</p>
+<p>The sum of the squares of the first ten natural numbers is 1^2 + 2^2 + ... + 10^2 = 385</p>
+<p>The square of the sum of the first ten natural numbers is (1 + 2 + ... + 10)^2 = 55^2 = 3025</p>
 <p>
 	Hence the difference between the sum of the squares of the first ten natural numbers and the
 	square of the sum is 3025 - 385 = 2640.
