269406775f3f25611cbaa3fc0f85a5c4bd29a937
Jeffrey Porter <jmilesporter@gmail.com>
2022-09-23 18:18:35 -0400

    Problems 8-10.

---
+++ src/routes/(problems)/problem-10/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+	function isPrime(n) {
+		if (n < 2) {
+			return false;
+		}
+		for (let i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i === 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	// given a natural number 'n', determine
+	// if the number is prime or not
+	let primes = [];
+	function countPrimes() {
+		for (let i = 2; i <= 2000000; i++) {
+			if (isPrime(i)) {
+				primes.push(i);
+			}
+		}
+
+		return primes;
+	}
+	function sum(numbers) {
+		return numbers.reduce((a, b) => a + b, 0);
+	}
+
+	let answer = sum(countPrimes());
+</script>
+
+<h1>Problem 10</h1>
+
+<p>The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.</p>
+<p>Find the sum of all the primes below two million.</p>
+
+<p class="answer">{answer}</p>
+
+<style>
+	h1 {
+		text-align: center;
+		margin: 1.5em;
+	}
+	p {
+		text-align: center;
+	}
+	.answer {
+		text-align: center;
+		font-size: xxx-large;
+		align-items: center;
+	}
+</style>
---
+++ src/routes/(problems)/problem-9/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+	function pythagoreanTriplet() {
+		for (let a = 1; a < 1000; a++) {
+			for (let b = 1; b < 1000; b++) {
+				for (let c = 1; c < 1000; c++) {
+					if (a + b + c === 1000 && a < b && b < c && a * a + b * b === c * c) {
+						return a * b * c;
+					}
+				}
+			}
+		}
+	}
+
+	let answer = pythagoreanTriplet();
+</script>
+
+<h1>Problem 9</h1>
+
+<p>
+	A Pythagorean triplet is a set of three natural numbers, <var>a</var> &lt; <var>b</var> &lt;
+	<var>c</var>, for which,
+</p>
+<p>
+	<var>a</var><sup>2</sup> + <var>b</var><sup>2</sup> = <var>c</var><sup>2</sup>
+</p>
+<p>For example, 3<sup>2</sup> + 4<sup>2</sup> = 9 + 16 = 25 = 5<sup>2</sup>.</p>
+<p>
+	There exists exactly one Pythagorean triplet for which <var>a</var> + <var>b</var> + <var>c</var>
+	= 1000.<br />Find the product <var>abc</var>.
+</p>
+
+<p class="answer">{answer}</p>
+
+<style>
+	h1 {
+		text-align: center;
+		margin: 1.5em;
+	}
+	p {
+		text-align: center;
+	}
+	.answer {
+		text-align: center;
+		font-size: xxx-large;
+		align-items: center;
+	}
+</style>
